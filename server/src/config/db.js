c585fe542cf5a21430bc7203bf8ede47ebf5fe1a
ashif <hasanat153@gmail.com>
2025-08-26 19:03:31 +0600

    uri

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/courier_db';
-await mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("Mongo error =>", err.message));
+  const uri = process.env.MONGO_URI;
+  //|| 'mongodb://127.0.0.1:27017/courier_db';
+  await mongoose
+    .connect(uri)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("Mongo error =>", err.message));
 };
 export default connectDB;
